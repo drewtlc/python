0fd1defde78fc656169361aa9c062f3bfe3287e3
drew <drewtlc@yandex.ru>
2019-06-17 16:50:10 +0300

    Чтение бинарных данных в правильном порядке байт

--- UK/load_data.py
+++ UK/load_data.py
@@ -6,36 +6,43 @@ import re
 import struct
 
 class LoadData:
+    # Подключение к MSSQL
     def connect(self, server, username, password, database):
         conn =  ms.connect(server, username, password, database)
         return conn
 
+    # Выполнение SQL запроса к БД
     def readSql(self, conn, sql):
         dataframe = pd.read_sql(sql, conn)
         return dataframe
 
+    # Чтение таблицы из БД (через выполнение запроса)
     def readTable(self, conn, tableName, conditions=""):
         sql = "select * from " + tableName
         if conditions != "":
             sql = sql + " where " + conditions
         return self.readSql(conn, sql)
 
+    # Наложение фильтра по вхождению строк в значение колонки. Истина, если есть вхождение хотя бы одной строки.
     def filterByStrOr(self, df, colname, strings):
         return df[df[colname].notna() & reduce(lambda x,y: x|y, map(lambda str: df[colname].str.contains(str, flags = re.IGNORECASE), strings))]
 
+    # Наложение фильтра по вхождению строк в значение колонки. Истина, если есть вхождение всех строк.
     def filterByStrAnd(self, df, colname, strings):
         return df[df[colname].notna() & reduce(lambda x,y: x&y, map(lambda str: df[colname].str.contains(str, flags = re.IGNORECASE), strings))]
 
+    # Перевод бинарного поля в массив чисел float. На число выделяется 4 байта, которые меняют свой порядок на обратный перед переводом во float.
     def toFloatArray(self, binData):
         result=[]
         if type(binData)==bytes:
-            blockSize = 4
+            blockSize = 4            
             for i in range(0,len(binData),blockSize):
                 block = binData[i:i+blockSize]
-                fl = struct.unpack(">f", block)
+                fl = struct.unpack(">f", block[::-1])
                 result.append(fl[0])
         return result
 
+    # Построение DataFrame по списку колонок keyColumns
     def createArrayDataFrame(self, df, keyColumns, arrayColumn):
         data = dict()
         maxlen = 0
@@ -47,6 +54,9 @@ class LoadData:
             while len(val)<maxlen:
                 val.append(0)
         return pd.DataFrame.from_dict(data)
+
+#    def toString(self, floatArray):
+#        return reduce(lambda s,f: (s if (s=="") else s+",") + str(f), floatArray, "")
 
 def readTables():
     ld = LoadData()
@@ -78,18 +88,19 @@ def readTables():
     # Обработка данных измерений
     p420_1["DynamicDataArray"] = p420_1["DynamicData"].apply(ld.toFloatArray)
     p420_1_data = ld.createArrayDataFrame(p420_1, ["idMeasure", "Name", "Date"], "DynamicDataArray")
-    print(p420_1_data.head(10))
     p420_1["DynamicDataArrayLen"] = p420_1["DynamicDataArray"].apply(len).astype("int")
-    p420_1 = p420_1.drop(["DynamicData", "DynamicDataArray"], axis=1)
 
-    print(p420_1.head())
+    pd.options.display.max_colwidth = 130
+    print(p420_1.head()[["idMeasure", "Name", "Date", "DynamicDataArray"]])
 
     print("Запись в файл...")
+    p420_1 = p420_1.drop(["DynamicData"], axis=1)
     #p420_1.to_csv("~/Share/p420_1.csv", index=False)
     #p420_1_data.to_csv("~/Share/p420_1_data.csv", index=False)
     with pd.ExcelWriter("/home/drew/Share/p420_1.xlsx") as writer:
         p420_1.to_excel(writer, sheet_name="p420_1", index=False)
         p420_1_data.to_excel(writer, sheet_name="p420_1_data", index=False)
         writer.save()
+    print("Готово")
 
 readTables()
